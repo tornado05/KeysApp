676589e62d341fc8a24a477d7a39b8dfc58c54f1
anonymous <user@step.itstep.rv.ua>
2016-08-13 12:54:33 +0300

    adds model init

--- src/public/js/views/AppView.js
+++ src/public/js/views/AppView.js
@@ -6,10 +6,20 @@ app.AppView = Backbone.View.extend({
 
 	template: templates.get('Main'),
 
-	version: '0.2.1',
+	version: '0.0.0',
+
+	workers: new app.Workers(),
 
 	initialize() {
 		console.log('constructor');
+		console.log(new app.Workers({test: "test"}));
+		console.log("=====================================================================================");
+		var worker = new app.Workers({id: 1, worker_name: "Maria Pavlovna"});
+		console.log(worker);
+		console.log(worker.get("id"));
+		console.log(worker.set("id", 2));
+		console.log(worker.get("id"));
+		console.log(worker.getName());
 		this.render();
 	},
 
